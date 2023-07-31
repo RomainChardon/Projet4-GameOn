d04b32196db2846ba900dcd6c46fb4944f12e610
RomainChardon <chardon.romain26@gmail.com>
2023-07-31 10:31:11 +0200

    style erreur, pb regex

--- starterOnly/formValidator.js
+++ starterOnly/formValidator.js
@@ -1,12 +1,7 @@
 const form = document.querySelector('form');
 
-
-
 form.addEventListener("submit", (event) => {
     
-    // empeche le refresh
-    event.preventDefault();
-
     // select input
     let prenom = document.getElementById("first");
     let nom = document.getElementById("last");
@@ -14,10 +9,14 @@ form.addEventListener("submit", (event) => {
     let date = document.getElementById("birthdate");
     let nbTournois = document.getElementById("quantity");
     let ca = document.getElementById("checkbox1");
+    let formValide = true;
+    let regexMail = /[A-Za-z0-9]+\.[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]+/i;
+    let regexQuantity = /[0-9]+/i;
 
     if (prenom.value.length < 2) {
         prenom.classList.add("erreur");
         erreurAlert('Minimum 2 caractères !', prenom);
+        formValide = false;
     } else {
         prenom.classList.remove("erreur");
     }
@@ -25,8 +24,33 @@ form.addEventListener("submit", (event) => {
     if (nom.value.length < 2) {
         nom.classList.add("erreur");
         erreurAlert('Minimum 2 caractères !', nom);
+        formValide = false;
     } else {
         nom.classList.remove("erreur");
+    }
+
+    // if (regexMail.test(email)) {
+    //     email.classList.add("erreur");
+    //     erreurAlert('email non valide', email);
+    //     formValide = false;
+    //     console.log('blabla');
+    // } else {
+    //     email.classList.remove("erreur");
+    //     console.log('blablaOK');
+    // }
+
+    if (regexQuantity.test(nbTournois)) {
+        nbTournois.classList.add("erreur");
+        erreurAlert('Chiffre', nbTournois);
+        formValide = false;
+    } else {
+        nbTournois.classList.remove("erreur");
+    }
+
+    if (formValide == false) {
+        // empeche le refresh
+        event.preventDefault();
+        console.log('nope');
     }
 
 });
@@ -39,8 +63,8 @@ function erreurAlert(message, select) {
     let newText = document.createElement("p");
 
     newText.textContent = message;
-    newDiv.classList.add('error-container');
-    newText.classList.add('error');
+    newDiv.classList.add('erreur-container');
+    newText.classList.add('erreur-text');
 
     newDiv.appendChild(newText);
 
